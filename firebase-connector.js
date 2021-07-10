b2a85e9d2988121488c4a72a7e197d0cbb049398
qianruzh <qianruzzz@gmail.com>
2021-07-10 15:11:04 +0800

    revise

--- firebase-connector.js
+++ firebase-connector.js
@@ -1,6 +1,6 @@
   // Your web apps Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  var firebaseConfig = {
+var firebaseConfig = {
     apiKey: "AIzaSyA-wExMG-1qwzklCMtkgAbQfSBHOWbXZl8",
     authDomain: "best-learning-tool-ffcbb.firebaseapp.com",
     projectId: "best-learning-tool-ffcbb",
@@ -8,7 +8,22 @@
     messagingSenderId: "1095156666135",
     appId: "1:1095156666135:web:60a716e37125aec5b27152",
     measurementId: "G-F372F8FCJ0"
-  };
+};
   // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+
+firebase.database().ref();  // Specifies the database root
+firebase.database().ref("child/path") // Specifies a specific location in the database tree
+
+let dbLocation = firbase.database().ref('deck/flashcards');
+
+dbLocation.set(myFlashcards);
+
+// Make the database point to the location root -> deck -> flashcards
+// If the location doesn't exist is will be created
+firebase.database().ref('deck/flashcards'); 
+ 
+// Remove myFlashcards from the database
+firebase.database.remove()
