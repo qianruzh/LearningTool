08e8a4b245c4f86c09eaa6c89090da4cae744b02
qianruzh <qianruzzz@gmail.com>
2021-07-13 23:51:43 +0800

    test

--- firebase-connector.js
+++ firebase-connector.js
@@ -1,14 +1,3 @@
-  // Your web apps Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig = {
-    apiKey: "AIzaSyA-wExMG-1qwzklCMtkgAbQfSBHOWbXZl8",
-    authDomain: "best-learning-tool-ffcbb.firebaseapp.com",
-    projectId: "best-learning-tool-ffcbb",
-    storageBucket: "best-learning-tool-ffcbb.appspot.com",
-    messagingSenderId: "1095156666135",
-    appId: "1:1095156666135:web:60a716e37125aec5b27152",
-    measurementId: "G-F372F8FCJ0"
-};
   // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
